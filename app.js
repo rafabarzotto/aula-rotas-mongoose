cedbf15b4e6ae80452dc63d8d0b3f1e26f5fdc95
Rafael Barzotto <rafabarzotto@hotmail.com>
2015-10-24 12:34:21 -0200

    Separando os arquivos

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var Controller = {
   create: function(req, res) {
   var dados = {
@@ -69,112 +70,37 @@ var Controller = {
   }
 }
 
+
 var http = require('http');
 
-// Bring Mongoose into the app
-var mongoose = require('mongoose');
+var Controller = require('./controller')
 
-// Build the connection string
-mongoose.connect('mongodb://localhost/pos-unoesc');
-
-var db = mongoose.connection;
-
-// When successfully connected
-db.on('connected', function() {
-	console.log('Mongoose default connection open to ');
-});
-
-// If the connection throws an error
-db.on('error', function(err) {
-	console.log('Mongoose default connection error: ' + err);
-});
-
-// When the connection is disconnected
-db.on('disconnected', function() {
-	console.log('Mongoose default connection disconnected');
-});
-
-// When the connection is disconnected
-db.on('open', function() {
-	console.log('Mongoose default connection is open');
-});
-
-var Schema = mongoose.Schema;
-
-var json_schema = {
-	name: {
-		type: String,
-		default: ''
-	},
-	description: {
-		type: String,
-		default: ''
-	},
-	alcohol: {
-		type: Number,
-		min: 0
-	},
-	price: {
-		type: Number,
-		min: 0
-	},
-	category: {
-		type: String,
-		default: ''
-	}
-}
-
-var BeerSchema = new Schema(json_schema);
-
-var Beer = mongoose.model('Beer', BeerSchema);
-
-var msg = '';
-
-http.createServer(function(req, res) {
-
-	var url = req.url;
-
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'
-	});
-
-	var dados = {
-		name: 'Skol',
-		description: 'Desce redondo',
-		alcohol: 10,
-		price: 12,
-		category: 'pilsen'
-	};
-	var model = new Beer(dados);
-
-	/*
-		model.save(function(err, data) {
-			if (err) {
-				console.log('Erro', err);
-				msg = 'Erro' + err;
-			}
-			console.log('Cerveja Inserida', data);
-			msg = 'Cerveja Inserida' + data;
-		});
-		res.end(msg);*/
-
-
-	switch (url) {
-		case '/api/beers/create':
-			Controller.create(req, res);
-			break;
-		case '/api/beers/save':
-			Controller.save(req, res);
-			break;
-		case '/api/beers/retrieve':
-			Controller.update(req, res);
-			break;
-		case '/api/beers/delete':
-			Controller.delete(req, res);
-			break;
-		default:
-			res.end('erro url', url);
-	}
-
+http.createServer(function (req, res) {
+  console.log('url: ', req.url);
+  var url = req.url;
+  
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  switch(url) {
+    case '/create':
+      Controller.create(req, res);
+      break;
+    case '/retrieve':
+      Controller.retrieve(req, res);
+      break;
+    case '/update':
+      Controller.update(req, res);
+      break;
+    case '/delete': 
+      Controller.delete(req, res);
+      break;
+    default:
+      console.log("Nada a fazer");
+  }
+  
 }).listen(3000);
+
+
 console.log('Server running at http://localhost:3000/');
+
+console.log('Server running at http://localhost:3000/');
+
