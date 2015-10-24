43311d64ad4ad85070ad08136102d08fd7cfe155
Rafael Barzotto <rafabarzotto@hotmail.com>
2015-10-24 12:30:25 -0200

    Um arquivo só

--- app.js
+++ app.js
@@ -1,15 +1,72 @@
 var Controller = {
-	save: function(req, res) {
-		model.save(function(err, data) {
-			if (err) {
-				console.log('Erro', err);
-				msg = 'Erro' + err;
-			}
-			console.log('Cerveja Inserida', data);
-			msg = 'Cerveja Inserida' + data;
-		});
-		res.end(msg);
-	}
+  create: function(req, res) {
+  var dados = {
+      name: 'Skol',
+      description: 'desce redondo',
+      alcohol: 24,
+      price: 30,
+      category: 'pilsen'
+    };
+
+    var model = new Model(dados);
+    var msg = '';
+
+    model.save(function (err, data) {
+      if(err) {
+        console.log("Erro: ", err);
+        msg = "Erro: " + err;
+      } else {
+        console.log("Cerveja inserida.", data);
+        msg = "Cerveja inserida. " + data;
+      }
+      res.end(msg);
+    });
+  },
+  retrieve: function(req, res) {
+    var query = {};
+    Model.find(query, function(err, data) {
+      if(err) {
+        console.log("Erro: ", err);
+        msg = "Erro: " + err;
+      } else {
+        console.log("listagem: ", data);
+        msg = "Listagem: " + data;
+      }
+      res.end(msg);
+    });
+  },
+  update: function(req, res) {
+    var query = {name: /Skol/i};
+    var mod = {
+      name: 'Brahma',
+      alcohol: 12,
+      price: 130,
+      category: 'pilsen'
+    };
+    Model.update(query, mod, function(err, data) {
+      if(err) {
+        console.log("Erro: ", err);
+        msg = "Erro: " + err;
+      } else {
+        console.log("Cervejas atualizadas: ", data);
+        msg = "Cervejas atualizadas: " + data;
+      }
+      res.end(msg);
+    });
+  },
+  delete: function(req, res) {
+    var query = {name: /Skol/i};
+    Model.remove(query, function(err, data) {
+      if(err) {
+        console.log("Erro: ", err);
+        msg = "Erro: " + err;
+      } else {
+        console.log("Cerveja deletadas, quantidade: ", data.result);
+        msg = "Cerveja deletadas, quantidade: " + data.result;
+      }
+      res.end(msg);
+    });
+  }
 }
 
 var http = require('http');
