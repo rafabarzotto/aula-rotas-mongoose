cd69ed84f0c6e7ffdb3a047c5ac64bdeb675a540
Rafael Barzotto <rafabarzotto@hotmail.com>
2015-10-24 12:02:48 -0200

    Traduz cm/s

--- exer3.js
+++ exer3.js
@@ -8,22 +8,22 @@ var db = mongoose.connection;
 
 // When successfully connected
 db.on('connected', function () {
-  console.log('Mongoose default connection open to ');
+  console.log('Mongoose Conectado! ');
 });
 
 // If the connection throws an error
 db.on('error',function (err) {
-  console.log('Mongoose default connection error: ' + err);
+  console.log('Mongoose ERRO: ' + err);
 });
 
 // When the connection is disconnected
 db.on('disconnected', function () {
-  console.log('Mongoose default connection disconnected');
+  console.log('Mongoose disconectado!');
 });
 
 // When the connection is disconnected
 db.on('open', function () {
-  console.log('Mongoose default connection is open');
+  console.log('Mongoose conexão aberta!');
 });
 
 var Schema = mongoose.Schema;
